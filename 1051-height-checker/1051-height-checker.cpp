f142bf6fc33be785516e2014d3d6cf2eb46575c5
palabhradeep635-star <palabhradeep635@gmail.com>
2026-06-23 12:20:09 +0530

    Time: 0 ms (100%), Space: 11.5 MB (32.5%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+       vector<int>exp=heights;
+       int count=0;
+       sort(exp.begin(),exp.end());
+       for(int i=0;i<heights.size();i++){
+        if(heights[i]!=exp[i]){
+            count++;
+        }
+       } 
+       return count;
+    }
+};

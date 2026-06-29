b0cc9ea9bd2b7a6fed935ad914cd6bed163b3d8d
palabhradeep635-star <palabhradeep635@gmail.com>
2026-06-29 19:53:00 +0530

    Time: 5 ms (19.25%), Space: 20.4 MB (98.81%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-      int ans= 0;
-      for(int val:nums){
-      ans=ans^val;  
-      }
-    return ans;
-    }
+        sort(nums.begin(), nums.end());
 
+        int n = nums.size();
+
+        for (int i = 0; i < n - 1; i += 2) {
+            if (nums[i] != nums[i + 1])
+                return nums[i];
+        }
+
+        return nums[n - 1];
+    }
 };
